a5c87f13a4cae223a30d57565d57ab26d61f9328
Alaa Alharthi <72317449+alaasas@users.noreply.github.com>
2021-12-01 20:06:15 -0500

    Update game DB

--- game.sql
+++ game.sql
@@ -2,16 +2,7 @@ DROP DATABASE IF EXISTS game;
 CREATE DATABASE game;
 USE game;
 
-DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS users;
-
-CREATE TABLE games (
-    ID int NOT NULL auto_increment,
-    name VARCHAR(50),
-    platform VARCHAR(50),
-    description VARCHAR(2000),
-    PRIMARY KEY(ID)
-);
 
 CREATE TABLE users (
     fullname VARCHAR(50),
