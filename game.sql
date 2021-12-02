3e0e6e25171204bc317c238da3363a96e37ed287
Alaa Alharthi <72317449+alaasas@users.noreply.github.com>
2021-12-01 20:03:03 -0500

    Update game db

--- game.sql
+++ game.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS GameDB;
-CREATE DATABASE GameDB;
-USE GameDB;
+DROP DATABASE IF EXISTS game;
+CREATE DATABASE game;
+USE game;
 
 DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS users;
@@ -19,11 +19,3 @@ CREATE TABLE users (
     emailid VARCHAR(100),
     PRIMARY KEY(emailid)
 );
-
-INSERT INTO games(name,platform,description) VALUE ('Kingdom Hearts','PlayStation 2','Kingdom Hearts is a series of action role-playing games developed and published by Square Enix.');
-
-INSERT INTO users VALUE ('John Adam','ABCabc123$','john.adam@outlook.com');
-
-Select * from games;
-
-Select * from users;
